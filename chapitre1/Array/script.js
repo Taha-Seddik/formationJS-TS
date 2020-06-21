9ffcfca3f6102b66a671f029e5f663bbc6cc3dd6
taha <taha.seddik1992@gmail.com>
2020-06-21 17:43:46 +0200

    chapitre 1 exercice array

---
+++ chapitre1/Array/script.js
@@ -0,0 +1,15 @@
+var transactions = [];
+
+// Init app
+function init() {
+  transactions.push("virement d'un client ");
+  transactions.push("payer facture d'eau'");
+  transactions.push("acheter un pc");
+  transactions.push("payer facture de gaz ");
+  console.log(transactions);
+
+  // ici des exemple de gestion du tableau
+}
+
+// call init
+init();

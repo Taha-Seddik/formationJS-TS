b3c037db5fb8fea0ee315fab0d6d2645527b3609
taha <taha.seddik1992@gmail.com>
2020-06-21 20:23:09 +0200

    events

--- chapitre2/script.js
+++ chapitre2/script.js
@@ -1,4 +1,40 @@
-// Init app
-function init() {}
+var possibleTransactions = [
+  "chair",
+  "pancake",
+  "bunny",
+  "carrot",
+  "potato",
+  "salad",
+  "curtains",
+  "snake",
+  "apples",
+  "ice cream",
+];
 
-init();
+function generateTaskName() {
+  var i = Math.floor(Math.random() * possibleTransactions.length);
+  return possibleTransactions[i];
+}
+
+function generateAmount() {
+  return Math.floor(Math.random() * 100);
+}
+
+function createTransaction(name, amount) {
+  return new Transaction(name, amount);
+}
+
+var transactions = [];
+function addTransaction(event) {
+  var transaction = createTransaction(generateTaskName(), generateAmount());
+  transactions.push(transaction);
+  console.log(transactions);
+}
+
+const addTransactionBtn = document.getElementById("addTransactionBtn");
+addTransactionBtn.addEventListener("click", addTransaction);
+
+const listnerDestroyerBtn = document.getElementById("listnerDestroyerBtn");
+listnerDestroyerBtn.addEventListener("click", function (e) {
+  addTransactionBtn.removeEventListener("click", addTransaction);
+});

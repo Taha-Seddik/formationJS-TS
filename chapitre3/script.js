99561fcd0f3cd52eac3570a68fc32360b1ed64e0
taha <taha.seddik1992@gmail.com>
2020-06-21 21:19:54 +0200

    form

--- chapitre3/script.js
+++ chapitre3/script.js
@@ -1,4 +1,32 @@
-// Init app
-function init() {}
+var transactions = [];
 
-init();
+// UI DOM ref
+const form = document.getElementById("form");
+const transactionNameInput = document.getElementById("name");
+const amountInput = document.getElementById("amount");
+
+function createTransaction(name, amount) {
+  return new Transaction(name, amount);
+}
+
+// Init app
+function addTransaction(e) {
+  // preventDefault to prevent the default form action for Ajax form submissions
+  e.preventDefault();
+
+  if (transactionNameInput.value.trim() === "" || amountInput.value.trim() === "") {
+    alert("Merci de remplir les champs");
+  } else {
+    var transaction = createTransaction(transactionNameInput.value, amountInput.value);
+    transactions.push(transaction);
+    console.log(transactions);
+    // clear inputs
+    transactionNameInput.value = "";
+    amountInput.value = "";
+  }
+}
+
+// Preuve de rechargement si on fait pas prevent default
+// console.log("j'été chargé une autre fois");
+
+form.addEventListener("submit", addTransaction);
